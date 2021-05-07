4290eb3ec1ea147f68b40c392fab62636168386c
Marycorreia12 <correia_nety@hotmail.com>
2021-05-07 19:39:05 +0100

    First homolog

--- Backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
+++ Backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -2,12 +2,11 @@ package com.devsuperior.dsvendas.services;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.entities.dto.SaleDTO;
@@ -24,19 +23,19 @@ public class SaleService {
 	
 	private SellerRepository sellerRepository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<SaleSumDTO> amountGroupedBySeller() {
 		return repository.amountGroupBySeller();
 	}
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<SaleSuccessDTO> successGroupBySeller() {
 		return repository.successGroupBySeller();
 	}
